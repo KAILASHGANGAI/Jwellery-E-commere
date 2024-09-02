d71f6296487af2fe4afaeaddedff47cbee055f8d
kailashgangai <karankumarnepal7@gmail.com>
2024-09-02 21:49:03 +0545

    our branches section added

---
+++ resources/views/products/branches.blade.php
@@ -0,0 +1,67 @@
+ <!-- product section area starts  -->
+ <section class="product_section p_section1 product_black_section bottom">
+     <div class="container">
+         <div class="row">
+             <div class="col-12">
+                 <div class="section_title">
+                     <h2>Our Branches</h2>
+                 </div>
+             </div>
+             <div class="col-12">
+                 <div class="product-area">
+                     <div class="row">
+                         <div class="col-sm-4">
+                             <div class=" branch-card">
+                                 <img src="https://via.placeholder.com/800x400" alt="Branch Image"
+                                     class="card-img-top branch-image">
+                                 <div class="card-body branch-content">
+                                     <h5 class="card-title">Branch Location</h5>
+                                     <p class="branch-location">
+                                         <strong>Address:</strong> 123 Main Street, Hometown, Country<br>
+                                         <strong>Contact:</strong> (123) 456-7890 <br>
+                                         <strong>Hours:</strong> 9:00 AM - 5:00 PM
+                                     </p>
+                                     <a href="https://www.google.com/maps/place/123+Main+Street,+Hometown,+Country"
+                                         target="_blank" class="btn btn-outline-warning">Map</a>
+                                 </div>
+                             </div>
+                         </div>
+                         <div class="col-sm-4">
+                             <div class=" branch-card">
+                                 <img src="https://via.placeholder.com/800x400" alt="Branch Image"
+                                     class="card-img-top branch-image">
+                                 <div class="card-body branch-content">
+                                     <h5 class="card-title">Branch Location</h5>
+                                     <p class="branch-location">
+                                         <strong>Address:</strong> 123 Main Street, Hometown, Country<br>
+                                         <strong>Contact:</strong> (123) 456-7890 <br>
+                                         <strong>Hours:</strong> 9:00 AM - 5:00 PM
+                                     </p>
+                                     <a href="https://www.google.com/maps/place/123+Main+Street,+Hometown,+Country"
+                                         target="_blank" class="btn btn-outline-warning">Map</a>
+                                 </div>
+                             </div>
+                         </div>
+                         <div class="col-sm-4">
+                             <div class=" branch-card">
+                                 <img src="https://via.placeholder.com/800x400" alt="Branch Image"
+                                     class="card-img-top branch-image">
+                                 <div class="card-body branch-content">
+                                     <h5 class="card-title">Branch Location</h5>
+                                     <p class="branch-location">
+                                         <strong>Address:</strong> 123 Main Street, Hometown, Country<br>
+                                         <strong>Contact:</strong> (123) 456-7890 <br>
+                                         <strong>Hours:</strong> 9:00 AM - 5:00 PM
+                                     </p>
+                                     <a href="https://www.google.com/maps/place/123+Main+Street,+Hometown,+Country"
+                                         target="_blank" class="btn btn-outline-warning">Map</a>
+                                 </div>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </section>
+ <!-- product section area ends  -->
