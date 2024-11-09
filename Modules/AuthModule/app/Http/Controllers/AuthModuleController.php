01beef49afb4cc56aa1392c86be509e780e423b8
kailashgangai <karankumarnepal7@gmail.com>
2024-11-09 18:21:26 +0545

    ddhdd

--- Modules/AuthModule/app/Http/Controllers/AuthModuleController.php
+++ Modules/AuthModule/app/Http/Controllers/AuthModuleController.php
@@ -13,6 +13,7 @@ class AuthModuleController extends Controller
 {
     public function showLoginForm()
     {
+        // Render the login view
         return view('authmodule::auth.login');
     }
 
